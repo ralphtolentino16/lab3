af8f7f864200c28c955ffde6617c9499a53d0d3a
ralphtolentino16 <ralph.tolentino16@gmail.com>
2025-02-13 14:20:43 -0500

    Added lab3d

---
+++ lab3d.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Author ID: rltolentino
+
+import subprocess
+
+def free_space():
+    p = subprocess.Popen(['df -h | grep \'/$\' | awk \'{print $4}\''], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    output = p.communicate()
+    stdout = output[0].decode('utf-8').strip()
+    return stdout
+
+print(free_space())
+
+
+
+
